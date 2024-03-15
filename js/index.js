f6e81b20bbcbb06ceaefe892bd4ec8b590aa39de
aakash <aakashxettri98@gmail.com>
2024-03-15 09:28:42 +0545

    worked on the counter and some js

---
+++ js/index.js
@@ -0,0 +1,26 @@
+// getting the counter element
+let countElement = document.getElementById("count");
+//getting the increment and decrement buttons
+let incrementButton = document.getElementById("increment");
+var decrementButton = document.getElementById("decrement");
+//inital count
+let count = 0;
+//function to update the count display
+function updateCount() {
+   countElement.textContent = count;
+}
+
+//event listener for the increment button
+incrementButton.addEventListener("click", () => {
+    count++;
+    updateCount();
+});
+
+//event listener for the  decrement button
+decrementButton.addEventListener("click", () => {
+   count--;
+   updateCount();
+});
+
+//initial update
+updateCount();
